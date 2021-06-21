6b0a5f6f1b581b0669f2a96c1edfc818c0e16f39
Waël <65678035+WaelBKZ@users.noreply.github.com>
2021-06-21 16:27:34 +0200

    Add files via upload
    changed the docstring format (Google -> Numpy) for better readability
    removed unused libraries imports

--- scrap.py
+++ scrap.py
@@ -1,4 +1,3 @@
-import sys
 import requests
 import datetime
 import time
@@ -6,6 +5,7 @@ import pandas as pd
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
+
 try:
     from simplejson.errors import JSONDecodeError
 except ImportError:
@@ -18,15 +18,24 @@ class ScrapAgreement:
 
     def __init__(self, agree_subject='tele', agree_type='title', token_legifrance=None):
         """ScrapAgreement class constructor.
-        Args:
-            agree_subject (str): Must be 'tele' for scrapping Telecommuting agreements or 'equa' for Professional
-            Equality agreements.
-            agree_type (str): Must be 'title' for scrapping agreements according to their title or 'theme' for scrapping
-            them according to their theme.
-            token_legifrance: Token obtained on Legifrance API. More details on how to get it in the Readme file.
-        Raises:
-            ValueError: If the agree_subject or agree_type are not in str format such as described above. If the user
-            has not entered a valid token_legifrance.
+
+        Parameters
+        ----------
+        agree_subject : str
+            Must be 'tele' for scrapping Telecommuting agreements or 'equa' for Professional Equality agreements.
+            (Default value = 'tele')
+        agree_type : str
+            Must be 'title' for scrapping agreements according to their title or 'theme' for scrapping them according to
+            their theme. (Default value = 'title')
+        token_legifrance : int_or_str
+            Token obtained on Legifrance API. More details on how to get it in the Readme file. (Default value = None)
+
+        Raises
+        ------
+        ValueError
+            If the agree_subject or agree_type are not in str format such as described above. Or if the user has not
+            entered a valid `token_legifrance`.
+
         """
         self.agree_subject = agree_subject
         self.agree_type = agree_type
@@ -64,7 +73,8 @@ class ScrapAgreement:
         self.token_legifrance = token_legifrance
 
     def get_max_page_number(self):
-        """Creates a max_page_number attribute, which corresponds to the number of pages for the search results on Legifrance.
+        """Creates a max_page_number attribute, which corresponds to the number of pages for the search results on
+        Legifrance.
         """
         first_page_url = self.url_scrap.format(page_number=1)
         wd = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
@@ -77,10 +87,17 @@ class ScrapAgreement:
     @staticmethod
     def get_a_page(soup):
         """Gets a list of dictionaries of agreements IDs and links on a given Legifrance page.
-        Args:
-            soup (BeautifulSoup object): The soup object of a given page of agreements from Legifrance.
-        Returns:
-            list: A list of dictionaries each containing an unique ID and link for each agreement on a chosen page.
+
+        Parameters
+        ----------
+        soup (BeautifulSoup object):
+            The soup object of a given page of agreements from Legifrance.
+
+        Returns
+        -------
+        list(dict)
+            A list of dictionaries each containing an unique ID and link for each agreement on a chosen page.
+
         """
         list_agreement_page = []
         for text in soup.findAll("h2"):
@@ -92,14 +109,20 @@ class ScrapAgreement:
         return list_agreement_page
 
     def get_all_pages_ids(self, n=1_000_000):
-        """Generalizes ScrapAgreement.get_a_page() method to all pages on Legifrance according to the scrapping criteria
-        chosen by the user (subject and type).
+        """Generalizes `ScrapAgreement.get_a_page()` method to all pages on Legifrance according to the scrapping
+        criteria chosen by the user (subject and type).
+
         Creates a list_agreement attribute (for your ScrapAgreement object) which consists in a list of dictionaries of
         IDs and links for each agreement.
-        They are fetched on chronological order.
-        Args:
-            n: Maximum number of pages that will be scrapped. Is set to 1 Million by default, i.e. it fetches every page
-            by default.
+
+        They are fetched in chronological order.
+
+        Parameters
+        ----------
+        n :
+            Maximum number of pages that will be scrapped. Is set to 1 Million by default, i.e. it fetches every page by
+            default. (Default value = 1_000_000)
+
         """
         if n == 1_000_000:
             # In case the user did not specify a number of page he is willing to scrap, we will automatically fetch
@@ -124,14 +147,24 @@ class ScrapAgreement:
     def get_company_size(siret):
         """Gets the size and the size category of a French company using its Siret, and by scrapping the Sirene website.
         Used website : https://www.sirene.fr/sirene/public/accueil
-        Args:
-            siret (int_or_str): Siret of the company you wish to get the size.
-        Returns:
-            str: Range of people in the company (e.g. '150 à 999 personnes').
-            str: Size category of the company, according to INSEE taxonomy ('PME', 'ETI' or 'GE'). For more, see :
-            https://www.insee.fr/fr/metadonnees/definition/c1057
+
+        Parameters
+        ----------
+        siret : int_or_str
+            Siret of the company you wish to get the size.
+
+        Returns
+        -------
+        str
+            Range of people in the company (e.g. '150 à 999 personnes').
+        str
+            Size category of the company, according to INSEE taxonomy ('PME', 'ETI' or 'GE'). For more, see :
+            https//www.insee.fr/fr/metadonnees/definition/c1057.
+
         """
-        url_siret = f'https://www.sirene.fr/sirene/public/recherche?recherche.sirenSiret=54565020200025&amp;recherche.commune=&amp;recherche.captcha=&amp;__checkbox_recherche.excludeClosed=true&amp;recherche.adresse=&amp;recherche.raisonSociale=&amp;sirene_locale=fr'
+        url_siret = f'https://www.sirene.fr/sirene/public/recherche?recherche.sirenSiret=54565020200025&amp;recherche' \
+                    f'.commune=&amp;recherche.captcha=&amp;__checkbox_recherche.excludeClosed=true&amp;recherche' \
+                    f'.adresse=&amp;recherche.raisonSociale=&amp;sirene_locale=fr '
         wd.get(url_siret)
         source = wd.page_source
         soup = BeautifulSoup(source)
@@ -148,48 +181,68 @@ class ScrapAgreement:
     @staticmethod
     def date_raw_to_dmy(date_raw):
         """Converts a timestamp to a day-month-Year date.
-        Args:
-            date_raw (int): timestamp (e.g. 1624028410).
-        Returns:
-            str: date with d-m-Y format.
+
+        Parameters
+        ----------
+        date_raw : int
+            timestamp (e.g. 1624028410).
+
+        Returns
+        -------
+        str
+            date with d-m-Y format.
+
         """
         your_dt = datetime.datetime.fromtimestamp(int(date_raw) / 1000)  # using the local timezone
         return your_dt.strftime("%d-%m-%Y")
 
     def get_agreements_infos(self, user_feedback=True, start_at=0):
         """Gets a complete set of information for each agreement on the chosen scope of agreements.
+
         Information that are gathered for each agreement are : dateDepot, dateDiffusion, dateEffet, dateFin, dateMaj,
-        dateTexte, company name, siret, size, size category, union, sector, nature, themes, NAF code and its content.
+        dateTexte, company name, siret, size, size category, union, sector, nature, themes, NAF code and the agreement
+        content.
+
         It enriches the list_agreement attribute with additional information using the Legifrance API:
         https://developer.aife.economie.gouv.fr/.
+
+        Parameters
+        ----------
+        user_feedback : :obj:`bool`, optional
+            Set to True if the user wishes to have feedback on the advancement of the scrapping. (Defaults to True)
+        start_at : :obj:`int`, optional
+            Number of the agreement you wish to start with for the scrapping. Useful when your session crashed and you
+            don't want to start again from the beginning. (Default value = 0)
+
+        Raises
+        ------
+        JSONDecodeError
+            An error occurs while you are trying to access the Legifrance API. The json decoding is not actually the
+            issue, it is rather that the API communication failed and that you most likely need to get a fresh token on
+            the API. This error can only be raised if the user decides to exit the program (by entering 'Enter' when
+            they are asked to).
+
+        Notes
+        -----
         Should only be ran after ScrapAgreement.get_all_pages_ids.
-        Args:
-            user_feedback (:obj:`bool`, optional): Set to True if the user wishes to have feedback on the advancement
-            of the scrapping. Defaults to True.
-            start_at (:obj:`int`, optional): Number of the agreement you wish to start with for the scrapping. Useful
-            when your session crashed and you don't want to start again from the beginning. Defaults to 0.
-        Raises:
-            JSONDecodeError: An error occurs while you are trying to access the Legifrance API. The json decoding
-            is not actually the issue, it is rather that the API communication failed and that you most likely need
-            to get a fresh token on the API. This error can only be raised if the user decides to exit the program (by
-            entering 'Enter' when they are asked to).
+
         """
         n = len(self.list_agreement)
-        if user_feedback == True:
+        if user_feedback:
             self.count_1 = 0
             count_2 = 0
         for i in range(start_at, n):
-            self.position = i  # Useful for recovering the position at which you stopped, in case your session crashed or
-            # raised an error.
+            self.position = i  # Useful for recovering the position at which you stopped, in case your session crashed
+            # or raised an error.
             agree = self.list_agreement[i]
             agreement_id = agree['id']
 
             headers = {'Authorization': f'Bearer {self.token_legifrance}'}
             data = {"id": agreement_id}
             count_requests_fail = 0
-            bool_break = 1 #When set to 1, the while loop below won't break unless we get a positive result or the user
-            # decides to stop the algorithm.
-            while bool_break :
+            bool_break = 1  # When set to 1, the while loop below won't break unless we get a positive result or the
+            # user decides to stop the algorithm.
+            while bool_break:
                 response = requests.post(
                     'https://sandbox-api.piste.gouv.fr/dila/legifrance-beta/lf-engine-app/consult/acco',
                     headers=headers,
@@ -274,34 +327,45 @@ class ScrapAgreement:
                 # moteur de recherche n'est pas égal à 1." error
                 print(f"Agreement {agreement_id}'s information couldn't be fetched at all.")
 
-            if user_feedback == True:
+            if user_feedback:
                 self.count_1 += 1
                 count_2 += 1
                 if count_2 == 500:
                     print(f"We're at {self.count_1 / n * 100}%.")
                     count_2 = 0
 
-    def auto_scrap(self, save_path=None, saving_format='xlsx', start_at = 0, skip_to_saving = False):
+    def auto_scrap(self, save_path=None, saving_format='xlsx', start_at=0, skip_to_saving=False):
         """Compiles all the scrapping methods in one. Does all the scrapping at once.
-        Args:
-            save_path (str): Path you wish the .csv database be saved to. Please remove any '\' or '/' at the end. For
-            example, it can be under the form of 'C:/User/documents/folder'.
-            saving_format (:obj:`str`, optional): 'xlsx' or 'csv'. Defaults as 'xlsx'.
-            start_at (:obj:`int`, optional): Number of the agreement you wish to start with for the scrapping. Useful
-            when your session crashed and you don't want to start again from the beginning. Defaults to 0. If set as
+
+        Parameters
+        ----------
+        save_path : str
+            Path you wish the .csv database be saved to. Please remove any '\' or '/' at the end. For example, it can be
+            under the form of `C :/User/documents/folder`. (Default value = None)
+        saving_format : :obj:`str`, optional
+            Should be set either as `xlsx` or `csv`. (Default value = `xlsx`)
+        start_at : :obj:`int`, optional
+            Number of the agreement you wish to start with for the scrapping. Useful
+            when your session crashed and you don't want to start again from the beginning. If set as
             higher than 0, will automatically skip the get_all_pages_ids method as we assume you've already finished it.
-            skip_to_saving (:obj:`bool`, optional): If, as a user, you only failed when entering the saving path and do
-            not wish to relaunch the whole thing. If set as True, will directly skip to the saving part. Defaults to
-            False.
-        Raises:
-            JSONDecodeError: An error occurs while you are trying to access the Legifrance API. The json decoding
+            (Default value = 0)
+        skip_to_saving : :obj:`bool`, optional
+            If, as a user, you only failed when entering the saving path and do not wish to relaunch the whole thing. If
+            set as True, will directly skip to the saving part. (Default value = False)
+
+       Raises
+        ------
+        JSONDecodeError
+            An error occurs while you are trying to access the Legifrance API. The json decoding
             is not actually the issue, it is rather that the API communication failed and that you most likely need
             to get a fresh token on the API. This error can only be raised if the user decides to exit the program (by
             entering 'Enter' when they are asked to).
-            FileNotFoundError: You entered a wrong save_path.
+        FileNotFoundError
+            You entered a wrong save_path.
+
         """
         time1 = time.time()
-        if skip_to_saving == False:
+        if skip_to_saving is False:
             if start_at == 0:
                 self.get_all_pages_ids()
                 print("All pages IDs fetched.")
@@ -309,15 +373,17 @@ class ScrapAgreement:
         self.df = pd.DataFrame(self.list_agreement)
         try:
             if saving_format == 'xlsx':
-                self.df.to_excel(save_path + f'/legifrance_database_{self.agree_subject}_{self.agree_type}.{saving_format}')
+                self.df.to_excel(
+                    save_path + f'/legifrance_database_{self.agree_subject}_{self.agree_type}.{saving_format}')
             elif saving_format == 'csv':
-                self.df.to_csv(save_path + f'/legifrance_database_{self.agree_subject}_{self.agree_type}.{saving_format}')
+                self.df.to_csv(
+                    save_path + f'/legifrance_database_{self.agree_subject}_{self.agree_type}.{saving_format}')
             else:
                 raise ValueError("Please choose 'xlsx' or 'csv' as the saving_format for your scrapped database.")
 
         except FileNotFoundError:
             raise FileNotFoundError('Please enter a valid save_path for your file. You do not need to relaunch the '
-                                    'whole process, please set skip_to_saving to True in the attributes to directly skip'
-                                    ' to saving the dataframe.')
+                                    'whole process, please set skip_to_saving to True in the attributes to directly'
+                                    'skip to saving the dataframe.')
 
         print(f"100%.\nScrapping done. It took {time.time() - time1} seconds.")
